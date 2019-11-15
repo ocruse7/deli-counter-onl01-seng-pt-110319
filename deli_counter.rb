71e32243ff8461ec4540175bbef2fc0d5e51b19b
Owen Cruse <ocruse7@gmail.com>
2019-11-15 05:00:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
-def line ( currentQueue )
+def line ( lineSize )
 
-  if currentQueue.size == 0
+  if lineSize.size == 0
     return puts "The line is currently empty."
   else
     index = 0
     message = "The line is currently: "
-    while index < currentQueue.size do
-      if index == (currentQueue.size - 1)
-        message += "#{index+1}. #{currentQueue[index]}"
+    while index < lineSize.size do
+      if index == (lineSize.size - 1)
+        message += "#{index+1}. #{lineSize[index]}"
       else
-        message += "#{index+1}. #{currentQueue[index]} "
+        message += "#{index+1}. #{lineSize[index]} "
       end
       index += 1
     end
@@ -18,7 +18,7 @@ def line ( currentQueue )
   puts message
 end
 
-def take_a_number(katz_deli, name)
+def take_a_number( katz_deli, name )
   
   queueNumber = katz_deli.size + 1
   katz_deli << name
@@ -26,7 +26,7 @@ def take_a_number(katz_deli, name)
   
 end
 
-def now_serving(katz_deli)
+def now_serving( katz_deli )
   
   if(katz_deli.size == 0)
     puts "There is nobody waiting to be served!"
